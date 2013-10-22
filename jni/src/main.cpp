0149f66bfb9788c4499f98aeaf2e47991583e1bc
Carles Pagès <page@cubata.homelinux.net>
2013-10-23 00:01:01 +0200

    Fix cleanup code.
    I was only calling the cleanup functions in case of exception. I also retabbed
    the file to use only spaces, and added the folders generated by ant to the
    ignore file.

--- jni/src/main.cpp
+++ jni/src/main.cpp
@@ -9,31 +9,31 @@
 #ifdef __ANDROID__
 #include <android/log.h>
 #define APPNAME "SDLApp"
-#define DIR_SEP	"/"
+#define DIR_SEP "/"
 #define DIR_CUR "/sdcard/"
 #define WIDTH 320
 #define HEIGHT 480
 #else
 #include  <iostream>
-#define DIR_SEP	"/"
-#define DIR_CUR	""
+#define DIR_SEP "/"
+#define DIR_CUR ""
 #define WIDTH 1280
 #define HEIGHT 800
 #endif
-#define DATAFILE(X)	DIR_CUR "data" DIR_SEP X
+#define DATAFILE(X) DIR_CUR "data" DIR_SEP X
 
 namespace
 {
-	SDL_Window *window = NULL;
-	SDL_GLContext context = NULL;
+    SDL_Window *window = NULL;
+    SDL_GLContext context = NULL;
 
-	std::string
-	fillSDLError(const std::string &base)
-	{
-		std::ostringstream oss;
-		oss << base << ": " << SDL_GetError() << std::endl;
-		return oss.str();
-	}
+    std::string
+    fillSDLError(const std::string &base)
+    {
+        std::ostringstream oss;
+        oss << base << ": " << SDL_GetError() << std::endl;
+        return oss.str();
+    }
 }
 
 const char vertex_src [] =
@@ -82,7 +82,7 @@ print_shader_info_log (
    if ( length ) {
       char* buffer  =  new char [ length ];
       glGetShaderInfoLog ( shader , length , NULL , buffer );
-	  std::cout << "shader info: " <<  buffer << std::flush;
+      std::cout << "shader info: " <<  buffer << std::flush;
       delete [] buffer;
  
       GLint success;
@@ -154,7 +154,7 @@ static GLuint make_shader(GLenum type, char *code)
     if (!shader_ok) {
         fprintf(stderr, "Failed to compile %d:\n", type);
 #ifdef __ANDROID__
-		__android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Failed to compile %d\n", type);
+        __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Failed to compile %d\n", type);
 #endif
         glDeleteShader(shader);
         return 0;
@@ -176,7 +176,7 @@ static GLuint make_program(GLuint vertex_shader, GLuint fragment_shader)
     if (!program_ok) {
         fprintf(stderr, "Failed to link shader program:\n");
 #ifdef __ANDROID__
-		__android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Failed to link shader prog\n");
+        __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Failed to link shader prog\n");
 #endif
         glDeleteProgram(program);
         return 0;
@@ -185,9 +185,9 @@ static GLuint make_program(GLuint vertex_shader, GLuint fragment_shader)
 }
 
 static const GLfloat g_vertex_buffer_data[] = { 
-	-1.0f, -1.0f, 0.0f,
-	 1.0f, -1.0f, 0.0f,
-	 0.0f,  1.0f, 0.0f,
+    -1.0f, -1.0f, 0.0f,
+     1.0f, -1.0f, 0.0f,
+     0.0f,  1.0f, 0.0f,
 };
 static GLuint program;
 
@@ -228,121 +228,120 @@ void  render()
 
 int main(int argc, char *argv[])
 {
-	try
-	{
-		srand(time(NULL));
+    try
+    {
+        srand(time(NULL));
 
-		if (SDL_Init(SDL_INIT_VIDEO) != 0)
-	   	{
+        if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        {
 #ifndef __ANDROID__
-			std::cout << "Couldn't initialize SDL: " << SDL_GetError()
-			   	<< std::endl;
+            std::cout << "Couldn't initialize SDL: " << SDL_GetError()
+                << std::endl;
 #endif
-			return -1;
-		}
+            return -1;
+        }
 
-		//ask for gles2
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_EGL, 1);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-		//SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+        //ask for gles2
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_EGL, 1);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+        //SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
 
-		window = SDL_CreateWindow("PCViewer", SDL_WINDOWPOS_UNDEFINED,
-				SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT,
-				SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN);
-		if (window == NULL)
-			throw std::runtime_error(fillSDLError("Failed to create window"));
+        window = SDL_CreateWindow("PCViewer", SDL_WINDOWPOS_UNDEFINED,
+                SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT,
+                SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN);
+        if (window == NULL)
+            throw std::runtime_error(fillSDLError("Failed to create window"));
 
-		context = SDL_GL_CreateContext(window);
-		if (context == NULL)
-			throw std::runtime_error(fillSDLError("Failed to create context"));
+        context = SDL_GL_CreateContext(window);
+        if (context == NULL)
+            throw std::runtime_error(fillSDLError("Failed to create context"));
 
-		//SDL_GL_SetSwapInterval(1);
+        //SDL_GL_SetSwapInterval(1);
 
-		int status = SDL_GL_MakeCurrent(window, context);
-		if (status) {
-			fprintf(stderr, "SDL_GL_MakeCurrent(): %s\n", SDL_GetError());
-		}
+        int status = SDL_GL_MakeCurrent(window, context);
+        if (status) {
+            fprintf(stderr, "SDL_GL_MakeCurrent(): %s\n", SDL_GetError());
+        }
 
-	   GLuint vertexShader   = load_shader ( vertex_src , GL_VERTEX_SHADER  );     // load vertex shader
-	   GLuint fragmentShader = load_shader ( fragment_src , GL_FRAGMENT_SHADER );  // load fragment shader
-	 
-	   GLuint shaderProgram  = glCreateProgram ();                 // create program object
-	   glAttachShader ( shaderProgram, vertexShader );             // and attach both...
-	   glAttachShader ( shaderProgram, fragmentShader );           // ... shaders to it
-	 
-	   glLinkProgram ( shaderProgram );    // link the program
-	   glUseProgram  ( shaderProgram );    // and select it for usage
-	 
-	   //// now get the locations (kind of handle) of the shaders variables
-	   position_loc  = glGetAttribLocation  ( shaderProgram , "position" );
-	   phase_loc     = glGetUniformLocation ( shaderProgram , "phase"    );
-	   offset_loc    = glGetUniformLocation ( shaderProgram , "offset"   );
-	   if ( position_loc < 0  ||  phase_loc < 0  ||  offset_loc < 0 ) {
-#ifndef __ANDROID__
-		   std::cerr << "Unable to get uniform location" << std::endl;
-#endif
-		  return 1;
-	   }
+        GLuint vertexShader   = load_shader ( vertex_src , GL_VERTEX_SHADER  );     // load vertex shader
+        GLuint fragmentShader = load_shader ( fragment_src , GL_FRAGMENT_SHADER );  // load fragment shader
+      
+        GLuint shaderProgram  = glCreateProgram ();                 // create program object
+        glAttachShader ( shaderProgram, vertexShader );             // and attach both...
+        glAttachShader ( shaderProgram, fragmentShader );           // ... shaders to it
+      
+        glLinkProgram ( shaderProgram );    // link the program
+        glUseProgram  ( shaderProgram );    // and select it for usage
+      
+        //// now get the locations (kind of handle) of the shaders variables
+        position_loc  = glGetAttribLocation  ( shaderProgram , "position" );
+        phase_loc     = glGetUniformLocation ( shaderProgram , "phase"    );
+        offset_loc    = glGetUniformLocation ( shaderProgram , "offset"   );
+        if ( position_loc < 0  ||  phase_loc < 0  ||  offset_loc < 0 ) {
+ #ifndef __ANDROID__
+            std::cerr << "Unable to get uniform location" << std::endl;
+ #endif
+           return 1;
+        }
 
 #if 0
-		GLuint vshader = make_shader(GL_VERTEX_SHADER, g_vshader);
-		GLuint fshader = make_shader(GL_FRAGMENT_SHADER, g_fshader);
-		program = make_program(vshader, fshader); 
-		if (program == 0) {
-			fprintf(stderr, "Error making program from shaders");
-		}
+        GLuint vshader = make_shader(GL_VERTEX_SHADER, g_vshader);
+        GLuint fshader = make_shader(GL_FRAGMENT_SHADER, g_fshader);
+        program = make_program(vshader, fshader); 
+        if (program == 0) {
+            fprintf(stderr, "Error making program from shaders");
+        }
 
-		GLuint vertexPosition_modelspaceID = glGetAttribLocation(program, "vertexPosition_modelspace");
+        GLuint vertexPosition_modelspaceID = glGetAttribLocation(program, "vertexPosition_modelspace");
 
-		GLuint vertexbuffer;
-		glGenBuffers(1, &vertexbuffer);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+        GLuint vertexbuffer;
+        glGenBuffers(1, &vertexbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-		glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glUseProgram(program);
+        glUseProgram(program);
 
-		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(vertexPosition_modelspaceID);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-			vertexPosition_modelspaceID, // The attribute we want to configure
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
-		);
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(vertexPosition_modelspaceID);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            vertexPosition_modelspaceID, // The attribute we want to configure
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+        );
 
-		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
 
-		glDisableVertexAttribArray(vertexPosition_modelspaceID);
+        glDisableVertexAttribArray(vertexPosition_modelspaceID);
 #endif
-		render();
+        render();
 
-		SDL_GL_SwapWindow(window);
+        SDL_GL_SwapWindow(window);
 
-		SDL_Delay(1000);
-
-		return 0;
-	}
-	catch (const std::exception &e)
-	{
+        SDL_Delay(1000);
+    }
+    catch (const std::exception &e)
+    {
 #ifndef __ANDROID__
-		std::cerr << "Error: " << e.what() << std::endl;
+        std::cerr << "Error: " << e.what() << std::endl;
 #endif
+    }
 
-		if (context)
-			SDL_GL_DeleteContext(context);
+    if (context)
+        SDL_GL_DeleteContext(context);
 
-		if (window)
-			SDL_DestroyWindow(window);
+    if (window)
+        SDL_DestroyWindow(window);
 
-		SDL_Quit();
-	}
-	return -1;
+    SDL_Quit();
+
+    return 0;
 }
